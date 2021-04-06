2536b2f7dda6d53641aad71757b888992c5f1484
Ilya Mikheew <ilya_mikheew@mail.ru>
2021-04-06 05:21:47 +0300

    NaN value fixed

--- task2/logic.js
+++ task2/logic.js
@@ -2,28 +2,46 @@ function quadEquation(){
 	var a = document.getElementsByName('A')[0].value;
 	var b = document.getElementsByName('B')[0].value;
 	var c = document.getElementsByName('C')[0].value;
-
-	var d = Math.pow(b, 2) - (4 * a * c);
-
-	if(d >= 0) {
-		var x1 = (-(b) + Math.sqrt(d))/(2 * a);
-		var x2 = (-(b) - Math.sqrt(d))/(2 * a);
-	} else {
-		x1 = "D < 0";
-		x2 = "D < 0";
-	}
-
 	var tabel1 = document.getElementById('tb1');
 	var row = document.createElement("TR");
-	row.classList.add("thDel")
+	row.classList.add("thDel");
 	row.onclick = function() {
 		this.parentNode.removeChild(row);
 	}
+	if(!(a == "" || b == "" || c == "")){
+		if(a == 0){
+			x1 = (-c)/b;
 
-	row.innerHTML = "<td>" + a + 
-				    "</td><td>" + b + 
-				    "</td><td>" + c + 
-				    "</td><td>" + x1 + 
-				    "</td><td>" + x2 + "</td>";
-	tabel1.appendChild(row);
+			if(isNaN(x1))
+				x1 = "-";
+
+			x2 = "-";
+			row.innerHTML = "<td>" + a + 
+			    "</td><td>" + b + 
+			    "</td><td>" + c + 
+			    "</td><td>" + x1 + 
+			    "</td><td>" + x2 + "</td>";
+			tabel1.appendChild(row);
+		}else{
+			var d = Math.pow(b, 2) - (4 * a * c);		
+			if(d >= 0) {
+				var x1 = (-(b) + Math.sqrt(d))/(2 * a);
+				var x2 = (-(b) - Math.sqrt(d))/(2 * a);
+
+				if(isNaN(x1))
+					x1 = "-";
+				if(isNaN(x2))
+					x2 = "-";
+			} else {
+				x1 = "D < 0";
+				x2 = "D < 0";
+			}
+			row.innerHTML = "<td>" + a + 
+			    "</td><td>" + b + 
+			    "</td><td>" + c + 
+			    "</td><td>" + x1 + 
+			    "</td><td>" + x2 + "</td>";
+			tabel1.appendChild(row);
+		}		
+	}
 }
